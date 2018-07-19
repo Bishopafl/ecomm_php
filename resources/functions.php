f44668c556b8cd4248efc99f81e26dc930106dd9
Adam Lopez <Adamf04@gmail.com>
2018-07-19 10:52:56 -0400

    added some helper functionality to functions.php to make code a lot cleaner.

--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,43 @@
 <?php 
 
+// Helper functions for our ecomm site
+// =========================================
+// |----------------------------------------
+// redirect functionality when we need it
+function redirect($location) {
+	header("location: $location ");
+}
+// =========================================
+// db query function
+// |----------------------------------------
+function query($sql) {
+	global $connection;
+	return mysqli_query($connection, $sql);
+}
+// =========================================
+// checks if there are results 
+// |----------------------------------------
+function confirm($result) {
+	if (!$result) {
+		die("QUERY FAILED " . mysqli_error($connection));
+	}
+}
+// =========================================
+// escape string to prevent SQL injections
+// |----------------------------------------
+function escape_string($string) {
+	global $connection;
+	return mysqli_real_escape_string($connection, $string);
+}
+// =========================================
+// mysqli fetch array 
+// |----------------------------------------
+function fetch_array() {
+	return mysqli_fetch_array($result);
+}
+// =========================================
+// 
+// |----------------------------------------
 
 
 
--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -1,8 +1,22 @@
 <div class="col-md-3">
     <p class="lead">Shop Name</p>
     <div class="list-group">
-        <a href="category.html" class="list-group-item">Category 1</a>
-        <a href="#" class="list-group-item">Category 2</a>
-        <a href="#" class="list-group-item">Category 3</a>
+
+
+		<?php 
+
+			$query = "SELECT * FROM categories";
+			$send_query = mysqli_query($connection, $query);
+
+			if (!$send_query) {
+				die("QUERY FAILED " . mysqli_error($connection));
+			}
+
+			while ($row = mysqli_fetch_array($send_query)) {
+				// display categories from db
+				echo "<a href='' class='list-group-item'>{$row['cat_title']}</a>";
+			}
+		?>
+	
     </div>
 </div>

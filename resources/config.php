da879fdcfc46855e152be7ccd3b60530b4b0acd2
Adam Lopez <Adamf04@gmail.com>
2018-07-19 10:10:00 -0400

    created sub-directories for easy organization of code.  makes displaying content much easier after chunking out the header, footer, sidenav, and slider pieces.

--- resources/config.php
+++ resources/config.php
@@ -1,7 +1,7 @@
 <?php
 // redirection helper 
 ob_start();
-// turns on the session in the browser
+// turns on the session in the browserob	
 session_start();
 
 // Defines DS as the directory separator
@@ -17,23 +17,24 @@ defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
 // displays file path
 // echo __FILE__;
 // |------------------------------------------
-
+// |========================================
 // using the magic constant directory, 
 // we define the routes for front and back end directories
 // |------------------------------------------
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
 defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK", __DIR__ . DS . "templates/back");
-
+// |========================================
 // setting up the database information constants so we 
-// can use them through-out our website
+// can use them through-out our website, requires 2 parameters
 // |------------------------------------------
-defined("DB_HOST") ? null : define("localhost");
-defined("DB_USER") ? null : define("root");
-defined("DB_PASS") ? null : define("");
-defined("DB_NAME") ? null : define("ecomm_db");
-
+defined("DB_HOST") ? null : define("DB_HOST", "localhost");
+defined("DB_USER") ? null : define("DB_USER", "root");
+defined("DB_PASS") ? null : define("DB_PASS", "");
+defined("DB_NAME") ? null : define("DB_NAME", "ecomm_db");
+// |========================================
 // connection to the database
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+// |========================================
 // functions can be available anywhere with this requirement
 require_once("functions.php");
 
